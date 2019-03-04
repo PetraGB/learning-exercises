e92c2ceda27f473ccf9b9f7baf3557699fd90d54
Petra Gama Broeken <petragb@gmail.com>
2019-03-04 19:22:30 +0100

    Corrected past code, added missing bonus exercise from day 5 and puppet css exercise.

--- day5.js
+++ day5.js
@@ -37,3 +37,20 @@ function invertCase(words) {
 }
 
 // ----------------------------------
+//  Write a constructor called Countdown that accepts a single argument - the number of seconds to count down. It should be possible to call the start method of instances of Countdown to initiate the countdown. Once the countdown starts, it should count down to zero starting with the number that was passed to the constructor and logging each number to the console with a one second delay.
+
+function Countdown(sec) {
+    this.sec = sec;
+    this.start = function() {
+        if (this.sec >= 0) {
+            setTimeout(
+                function() {
+                    console.log(this.sec);
+                    this.sec--;
+                    this.start();
+                }.bind(this), // bind because this does not work since it's 2 levels from actial this
+                1000
+            );
+        }
+    };
+}
