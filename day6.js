edc959e1b1613ab4ba3911d1bc783c45eb0ba111
Petra Gama Broeken <petragb@gmail.com>
2019-03-05 09:36:00 +0100

    Corrected DOM methods

--- day6.js
+++ day6.js
@@ -1,5 +1,5 @@
 function changeText(sel) {
-    var selected = document.getElementsByTagName(sel);
+    var selected = document.querySelectorAll(sel);
     for (var i = 0; i < selected.length; i++) {
         selected[i].style.fontStyle = "italic";
         selected[i].style.fontWeight = "bold";
@@ -11,7 +11,7 @@ function changeText(sel) {
 
 function turnIntoArray(classy) {
     var classArray = [];
-    var classOrigin = document.querySelectorAll(classy);
+    var classOrigin = document.getElementByClassName(classy);
     for (var i = 0; i < classOrigin.length; i++) {
         classArray.push(classOrigin[i]);
     }
@@ -24,6 +24,7 @@ function awesome() {
     var wow = document.createElement("div");
     var word = document.createTextNode("AWESOME");
     wow.appendChild(word);
+    // would also work with wow.innerText = "AWESOME" instead of line 25 and 26
     wow.style.position = "fixed";
     wow.style.zIndex = "2147483647";
     wow.style.left = "20px";
