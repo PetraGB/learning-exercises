6fa442ca6ebbb61d441f16231b61ffbe1808df18
Petra Gama Broeken <petragb@gmail.com>
2019-03-06 15:38:07 +0100

    Added menu

---
+++ sprint/script.js
@@ -0,0 +1,24 @@
+// document.documentElement.classList.add("on");
+
+var hamburger = document.getElementById("hamburger");
+var xButton = document.getElementById("x");
+var fullMenu = document.getElementById("hamburgerMenu");
+var menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", function(a) {
+    a.stopPropagation();
+    document.documentElement.classList.add("on");
+});
+
+x.addEventListener("click", function(a) {
+    document.documentElement.classList.remove("on");
+});
+
+menu.addEventListener("click", function(a) {
+    a.stopPropagation();
+});
+// so a click on menu does not bubble up to hamburgerMenu
+
+fullMenu.addEventListener("click", function(a) {
+    document.documentElement.classList.remove("on");
+});
